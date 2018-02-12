5d48696cc87df26a0afd07a6521372dd4b0b15c8
Claire Harris <c.harris.2@research.gla.ac.uk>
2018-02-12 13:49:34 +0000

    test-nc.jl: Code to implement GDAL changes. Can load in ERA interim t2m dataset, extract values for a set of plant species and compare these to the worldclim data.

--- examples/test-nc.jl
+++ examples/test-nc.jl
@@ -4,38 +4,26 @@ using Unitful.DefaultSymbols
 using AxisArrays
 using myunitful
 
+# Load example dataset downloaded from EMCWF
 ncinfo("/Users/claireh/Downloads/test.nc")
 
-x = ncread("/Users/claireh/Downloads/test.nc", "t2m")
+test = extractERA("/Users/claireh/Downloads/test.nc", "t2m",
+    collect(1.0month:1month:2month))
 
-y = x * 1.0
+### Load world temperature data downloaded from ERA interim ###
 
-y[y .≈ -32767] = NaN
-temps = y .* 0.0009362609303530759K .+ 270.9726153656286K
-
+# Basic info about data
 ncinfo("data/era_interim_moda_1990")
-lat = reverse(ncread("data/era_interim_moda_1990", "latitude"))
-lon = ncread("data/era_interim_moda_1990", "longitude")
-twomtemp = ncread("data/era_interim_moda_1990", "t2m")
-twomtemp = twomtemp * 1.0
+# Extract data for t2m parameter - temperature at 2m
+dir = "data/era_interim_moda_1990"
+tempax = extractERA(dir, "t2m", collect(1.0month:1month:10year))
 
-splitval = find(lon .== 180)[1]
-firstseg = collect((splitval+1):size(twomtemp,1))
-secondseg = collect(1:splitval)
-twomtemp = twomtemp[vcat(firstseg ,secondseg), :, :]
-lon[firstseg] = 180 - lon[firstseg]
-lon = lon[vcat(reverse(firstseg) ,secondseg)]
-
-twomtemp[twomtemp .≈ -32767] = NaN
-temps = twomtemp .* 0.0017312391138308897K .+ 270.9726153656286K
-tempsC = uconvert.(°C, temps)
-tempsC = ustrip.(tempsC)[:,end:-1:1, :]
-step1 = 0.75°
-step2 = 180°/241
-tempax = AxisArray(uconvert.(°C, temps),
-                       Axis{:longitude}(lon * °),
-                       Axis{:latitude}(lat * °),
-                       Axis{:time}(1month:1month:10year))
+# Collect lats and lons from array
+lon = ustrip(axes(tempax.array, 1).val)
+lat = ustrip(axes(tempax.array, 2).val)
+# Strip array of units to plot
+tempsC = ustrip(Array(tempax.array))
+# Plot array as pdf with world shapefile on top
 using RCall
 @rput tempsC
 @rput lat; @rput lon
@@ -47,12 +35,14 @@ image.plot(lon, lat, tempsC[,,1], xlab='', ylab='')
 plot(world, add = T)
 dev.off()"
 
+# Load data for land cover
 file = "data/World.tif"
 world = extractfile(file)
 wrld = ustrip.(world)
 @rput wrld
 R"image(wrld)"
 
+# Use to mask out oceans from t2m array and plot as PDF
 mask = isnan.(world)
 tempsC[find(mask)] = NaN
 @rput tempsC
@@ -65,11 +55,54 @@ image.plot(lon, lat, tempsC[,,1], xlab='', ylab='')
 plot(world, add = T)
 dev.off()"
 
+
+# Load GBIF data in order to extract t2m values from GPS locations
 using JuliaDB
 sol = load("/Users/claireh/Documents/PhD/Data/GBIF/Solanum/output")
+sol = pushcol(sol, :id, collect(1:215440))
 coords = select(sol, (:decimallatitude, :decimallongitude))
 x = select(coords, :decimallongitude); y = select(coords, :decimallatitude)
-yr = select(sol, :year); mn = select(sol, :month)
-vals = map(x * °, y * °, year) do x, y, yr
-    extractvalues(x, y, tempax, yr)
+years = select(sol, :year)
+vals = extractvalues(x * °, y * °, years, tempax, 1990)
+
+# Try plotting values for subset of species
+spp_names = ["Solanum dulcamara", "Solanum nigrum", "Solanum americanum",
+"Solanum parvifolium"]
+for i in spp_names
+spp = filter(p-> p[:species] == i, sol)
+ids = select(spp, :id)
+subvals = ustrip.(vals[ids])
+res = vcat(subvals...)
+res = res[!isnan.(res)]
+using RCall
+@rput res
+@rput i
+R"library(ggplot2);library(cowplot);library(gridExtra)
+q = qplot(as.vector(res), geom='histogram',
+            xlab ='Average temperature (deg C)')
+pdf(file=paste('plots/era_',i,'.pdf', sep=''), paper = 'a4r', height= 8.27, width=11.69 )
+print(q)
+dev.off()
+"
+end
+# Compare directly with worldclim
+dir = "/Users/claireh/Documents/PhD/Data/Worldclim/wc2.0_5m/wc2.0_5m_tavg"
+tavg = extractworldclim(dir)
+vals = extractvalues(x * °, y * °, tavg, 1month:1month:12month)
+for i in spp_names
+spp = filter(p-> p[:species] == i, sol)
+ids = select(spp, :id)
+subvals = ustrip.(vals[ids, :])
+res = vcat(subvals...)
+res = res[!isnan.(res)]
+using RCall
+@rput res
+@rput i
+R"library(ggplot2);library(cowplot);library(gridExtra)
+q = qplot(as.vector(res), geom='histogram',
+            xlab ='Average temperature (deg C)')
+pdf(file=paste('plots/worldclim_',i,'.pdf', sep=''), paper = 'a4r', height= 8.27, width=11.69 )
+print(q)
+dev.off()
+"
 end
