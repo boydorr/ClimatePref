0c416d855e82267a092206d29341a2cb97de8b60
Claire Harris <c.harris.2@research.gla.ac.uk>
2018-02-12 10:37:45 +0000

    Datatypes.jl: Created types for different climate data, with some checking

---
+++ src/DataTypes.jl
@@ -0,0 +1,29 @@
+
+abstract type AbstractClimate end
+
+mutable struct Worldclim <: AbstractClimate
+    array::AxisArray
+    function Worldclim(array::AxisArray)
+        size(array, 3) == 12 ||
+            error("There should be 12 months of data for worldclim")
+        new(array)
+    end
+end
+
+mutable struct Bioclim <: AbstractClimate
+    array::AxisArray
+    function Bioclim(array::AxisArray)
+        size(array, 3) == 9 ||
+            error("There should 9 climate variables for bioclim")
+        new(array)
+    end
+end
+
+mutable struct ERA <: AbstractClimate
+    array::AxisArray
+    function ERA(array::AxisArray)
+        typeof(collect(axes(array, 3).val)[1])<: Unitful.Time ||
+            error("Third dimension of array must be time")
+        new(array)
+    end
+end
