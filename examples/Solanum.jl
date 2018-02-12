5c3af76527a53cfa8eefd49f8c85d8ac3734ae04
Claire Harris <c.harris.2@research.gla.ac.uk>
2018-02-12 15:00:04 +0000

    Solanum.jl: Alter plotting of parameter histograms to reflect the new structure of the extracted values, i.e. now get an individual vector for each.

--- examples/Solanum.jl
+++ examples/Solanum.jl
@@ -48,55 +48,68 @@ na_max(x) = maximum(x[.!isnan.(x)])
 na_min(x) = minimum(x[.!isnan.(x)])
 na_var(x) = var(x[.!isnan.(x)])
 sol = pushcol(sol, :id, collect(1:215440))
-names = map(folders) do str
+names = map(searchdir(dir, "wc2.0_5m_")) do str
     Symbol(split(str, "wc2.0_5m_")[2])
 end
-for i in 1:length(names)
-    sol = pushcol(sol, names[i] , mapslices(na_mean, vals[i], 2)[:,1])
-end
+
+## Run through 4 tester species and plot histograms of 6 worldclim
+## variables for each
 spp_names = ["Solanum dulcamara", "Solanum nigrum", "Solanum americanum",
 "Solanum parvifolium"]
 for i in spp_names
-spp = filter(p-> p[:species] == i, sol)
-ids = select(spp, :id)
-res = map( x-> ustrip(vals[x][ids]), 1:7)
-using RCall
-@rput res
-@rput i
-R"library(ggplot2);library(cowplot);library(gridExtra)
-res[[2]][res[[2]]==-32768] = NA
-q2 = qplot(as.vector(res[[2]]), geom='histogram',
-            xlab ='Average precipitation (mm)')
-q3 = qplot(as.vector(res[[3]]), geom='histogram',
-            xlab ='Solar radiation (kJ m-2 day-1)')
-q4 = qplot(as.vector(res[[4]]), geom='histogram',
-            xlab ='Average temperature (°C)')
-q5 = qplot(as.vector(res[[5]]), geom='histogram',
-            xlab ='Maximum temperature (°C)')
-q6 = qplot(as.vector(res[[6]]), geom='histogram',
-            xlab ='Water vapour pressure (kPa)')
-q7 = qplot(as.vector(res[[7]]), geom='histogram',
-            xlab ='Wind speed (m s-1)')
-pdf(file=paste('plots/',i,'.pdf', sep=''), paper = 'a4r', height= 8.27, width=11.69 )
-grid.arrange(q2, q3, q4, q5, q6, q7, nrow=2)
-dev.off()
-"
+    # Select species and the id of the rows
+    spp = filter(p-> p[:species] == i, sol)
+    ids = select(spp, :id)
+    # Select values for row ids from each variable
+    res_prec = ustrip(vals_prec[ids])
+    res_srad = ustrip(vals_srad[ids])
+    res_tavg = ustrip(vals_tavg[ids])
+    res_tmax = ustrip(vals_tmax[ids])
+    res_vapr = ustrip(vals_vapr[ids])
+    res_wind = ustrip(vals_wind[ids])
+    # Combine into one array
+    res = [res_prec res_srad res_tavg res_tmax res_vapr res_wind]
+    using RCall
+    @rput res
+    @rput i
+    # Plot in R
+    R"library(ggplot2);library(cowplot);library(gridExtra)
+    res[, 1][res[ , 1]==-32768] = NA
+    q1 = qplot(as.vector(res[, 1]), geom='histogram',
+                xlab ='Average precipitation (mm)')
+    q2 = qplot(as.vector(res[, 2]), geom='histogram',
+                xlab ='Solar radiation (kJ m-2 day-1)')
+    q3 = qplot(as.vector(res[, 3]), geom='histogram',
+                xlab ='Average temperature (deg C)')
+    q4 = qplot(as.vector(res[, 4]), geom='histogram',
+                xlab ='Maximum temperature (deg C)')
+    q5 = qplot(as.vector(res[, 5]), geom='histogram',
+                xlab ='Water vapour pressure (kPa)')
+    q6 = qplot(as.vector(res[, 6]), geom='histogram',
+                xlab ='Wind speed (m s-1)')
+    pdf(file=paste('plots/new',i,'.pdf', sep=''), paper = 'a4r', height= 8.27, width=11.69 )
+    grid.arrange(q1, q2, q3, q4, q5, q6, nrow=2)
+    dev.off()
+    "
 end
+
 using RCall
-tavg = datasets[4]
-tavg = ustrip(tavg)
-@rput tavg
+tav = ustrip(tavg.array)
+@rput tav
 R"library(fields);
 for (i in 1:12){
-    jpeg(paste('plots/tavg', i, '.jpeg',sep=''), height= 595, width=842)
-    image.plot(tavg[,,i])
-    dev.off()}"
+    #jpeg(paste('plots/tavg', i, '.jpeg',sep=''), height= 595, width=842)
+    image.plot(tav[,,i])
+    #dev.off()
+    }"
+
 pts = [x[1:4],y[1:4]]
-thisstep = ustrip(step(axes(datasets[1], 1).val))
+thisstep = ustrip(step(axes(tavg.array, 1).val))
 @rput pts; @rput thisstep
 R"library(fields);
-jpeg('plots/points.jpeg', height= 595, width=842)
+#jpeg('plots/points.jpeg', height= 595, width=842)
 image.plot(seq(-180, 180, by=thisstep),
 seq(-90, 90, by=thisstep), tavg[,,1], xlab='', ylab='')
 points(pts[[1]], pts[[2]], pch=20)
-dev.off()"
+#dev.off()
+"
