c92f1a251a7dffa10a395fbf3bb3995f6af11c78
Claire Harris <c.harris.2@research.gla.ac.uk>
2018-04-17 13:52:40 +0100

    Testscript.jl: Test code for grouping GBIF occurrences by species

---
+++ examples/Testscript.jl
@@ -0,0 +1,10 @@
+# Add more workers if needed
+addprocs(8)
+
+using JuliaDB
+# Load data
+test = load("/Users/claireh/Documents/PhD/Data/GBIF/test/output")
+# Group by species, with count of each
+result = groupby(length, test, :species)
+# Filter out blank species names
+filter((x->x.species != ""), test)
