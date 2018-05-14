1c0333586ecd998b7ba18d551e434c404c758d5d
Claire Harris <c.harris.2@research.gla.ac.uk>
2018-05-14 15:23:49 +0100

    ClimatePref.jl: export new functions

--- src/ClimatePref.jl
+++ src/ClimatePref.jl
@@ -4,9 +4,13 @@ include("DataTypes.jl")
 export Worldclim, Bioclim, ERA, Reference
 
 include("GDAL.jl")
-export read, searchdir, extractworldclim, extractbioclim, extractERA, extractvalues
+export read, searchdir, extractworldclim, extractbioclim, extractERA,
+ extractvalues, extractfile
 
 include("Phylo_models.jl")
 export Brownian, Lambda, fitBrownian, fitLambda, varcovar
+
+include("drop_tip.jl")
+export getinternalnodes, drop_tip!
 
 end
