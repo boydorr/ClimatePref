571950c872945f71bbb60586bf2a3dbe6566c1cf
Claire Harris <c.harris.2@research.gla.ac.uk>
2018-05-14 14:59:31 +0100

    For_neil.jl: Comment out no longer needed code

--- examples/For_neil.jl
+++ examples/For_neil.jl
@@ -8,11 +8,12 @@ dict = Dict(zip(names,types))
 dict[:decimallatitude] = DataValues.DataValue{Float64}
 dict[:decimallongitude] = DataValues.DataValue{Float64}
 dict[:year] = DataValues.DataValue{Int64}
-dir = "/Users/claireh/Documents/PhD/Data/For_neil/finalL"
-liliales = loadtable(dir,
-        indexcols = [:phylum, :class, :order, :family, :genus, :species, :scientificname],
-        colparsers=dict)
-save(liliales, "/Users/claireh/Documents/PhD/Data/For_neil/Liliales")
+#dir = "/Users/claireh/Documents/PhD/Data/For_neil/finalL"
+#liliales = loadtable(dir,
+#        indexcols = [:phylum, :class, :order, :family, :genus, :species, :scientificname],
+#        colparsers=dict)
+#save(liliales, "/Users/claireh/Documents/PhD/Data/For_neil/Liliales")
+liliales = load("/Users/claireh/Documents/PhD/Data/For_neil/Liliales")
 species = loadtable("/Users/claireh/Documents/PhD/Data/For_neil/testspecies.csv",
 distributed = false)
 gen = collect(select(species, :Genus))
@@ -32,11 +33,12 @@ extras2 = map(x->string(split(x, " ")[1], " ", split(x, " ")[2], " ", split(x, "
 Synonym = map(x->split(x, " Synonym ")[1], syns)
 all_synonyms = vcat(Synonym, extras, extras2)
 
-dir = "/Users/claireh/Documents/PhD/Data/For_neil/finalA"
-asparagales = loadtable(dir,
-        indexcols = [:phylum, :class, :order, :family, :genus, :species, :scientificname],
-        colparsers=dict)
-save(asparagales, "/Users/claireh/Documents/PhD/Data/For_neil/Asparagales")
+#dir = "/Users/claireh/Documents/PhD/Data/For_neil/finalA"
+#asparagales = loadtable(dir,
+#        indexcols = [:phylum, :class, :order, :family, :genus, :species, :scientificname],
+#        colparsers=dict)
+#save(asparagales, "/Users/claireh/Documents/PhD/Data/For_neil/Asparagales")
+asparagales = load("/Users/claireh/Documents/PhD/Data/For_neil/Asparagales")
 without_syn = filter(x-> x.species in species_names, asparagales)
 total = merge(tab, collect(without_syn))
 using FileIO
@@ -48,7 +50,3 @@ syn_tab = collect(with_syn)
 with_syn = filter(x-> x.scientificname in all_synonyms, asparagales)
 total_syn = merge(syn_tab, collect(with_syn))
 FileIO.save("/Users/claireh/Documents/PhD/Data/For_neil/matches_with_synonyms.csv", total_syn)
-
-
-found_species = unique(select(total, :species))
-setdiff(species_names, found_species)
