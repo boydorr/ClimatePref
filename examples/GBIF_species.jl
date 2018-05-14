a876cf04d5a38e686c000339084e394e263077f7
Claire Harris <c.harris.2@research.gla.ac.uk>
2018-05-14 15:19:23 +0100

    GBIF_species.jl: more messy code for using JuliaDB on gbif and worldclim data

--- examples/GBIF_species.jl
+++ examples/GBIF_species.jl
@@ -1,8 +1,10 @@
 addprocs(8)
 @everywhere using JuliaDB
-gbif = load("output2")
+gbif = load("/Users/claireh/Documents/PhD/Data/GBIF/test/output")
+gbif = pushcol(gbif, :id, collect(1:25331))
 
 tb = groupby(length, gbif, :species)
+groupreduce(+, gbif, :species, select=:id)
 
 save(tb, "gbif_species")
 
@@ -52,3 +54,28 @@ save(test,  "/Users/claireh/Documents/PhD/Data/GBIF/test_copy/output")
 test = load("/Users/claireh/Documents/PhD/Data/GBIF/test_copy/output")
 test = pushcol(test, :id, collect(1:25331))
 result = groupby(length, test, :genus)
+
+
+addprocs(8)
+using JuliaDB
+gbif = load("GBIF_worldclim")
+tavg = load("tavg")
+dtavg = distribute(tavg, 8)
+gbif_tavg = join(gbif, dtavg, how=:left, lkey=:refval, rkey=:refval, rselect=:tavg)
+
+if typeof(select(red_genus, 2))<: Array{NamedTuples.NamedTuple, 1}
+    red_genus = table(select(red_genus, 1),
+    map(x-> collect(select(red_genus,2)[x])[1], 1:length(red_genus)),
+    names = [:species, :na_mean])
+end
+function na_mean(x, y)
+    if typeof(x) <: DataValues.DataValue x = x.value end
+    if typeof(y) <: DataValues.DataValue y = y.value end
+    if isnan(x) & isnan(y)
+        z = NaN32 *°C
+    else
+        vec = [x, y]
+        z = return mean(vec[.!isnan.(vec)])
+    end
+    return collect(z)[1]
+end
