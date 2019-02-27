9c9d3e08542cb7c9ba53e03058a08206a89dcf7c
Claire Harris <c.harris.2@research.gla.ac.uk>
2019-02-27 16:24:29 +0000

    For_met.jl: New simulations and plots.

--- examples/For_MET.jl
+++ examples/For_MET.jl
@@ -137,8 +137,8 @@ end
 
 ## 200,000 species over 625,000km² area
 numSpecies = 200000; grd = (10,10); req= 10.0kJ; individuals=5000000; area = (250.0*km)^2; totalK = 1000.0kJ/km^2
-#eco = create_eco(numSpecies, grd, area, totalK, req, individuals)
-#abun = runsim(eco, 2years, 1)
+eco = create_eco(numSpecies, grd, area, totalK, req, individuals)
+@time abun = runsim(eco, 1month, 1);
 #JLD.save("200000species.jld", "abun", abun)
 abun = JLD.load("data/200000species.jld", "abun")
 
@@ -148,4 +148,98 @@ plot_abun(abun, grd, 1:20, 1)
 # Plot species richness over time
 heatmap(mapslices(x->sum(x.>0), abun, dims = 1)[1,:,:,1], xlab = "Time (months)", ylab = "Space")
 
+# Animation abundance over time for first species
+for i in 1:24
+    display(heatmap(reshape(abun[1,:,i,1], 10, 10), clim = (0, 10)))
+end
+
+# Animation alpha bar and gamma
+for i in 1:24
+    meta = Metacommunity(abun[:,:,i,1])
+    b = reshape(subdiv(ᾱ(meta), 0)[:diversity], 10, 10)
+    B = metadiv(γ(meta), 0)[:diversity]
+    if i ==1
+        display(heatmap(b, layout = (2, 1), clim = (40000, 50000), title = "Subcommunity ᾱ"))
+    else
+        display(heatmap!(b, subplot = 1,  clim = (40000, 50000)))
+    end
+    display(plot!([i], B, seriestype = :scatter, subplot = 2, xlim = (0, 24), ylim = (199500, 200100), grid = false, label = "", color = :1, title = "Metacommunity γ"))
+end
+
 ## 200,000km² with 3000 species for 100 years (30 by 10)
+numSpecies = 3000; grd = (30,10); req = 100.0kJ; individuals = 1000000; area = 200000.0*km^2; totalK = 1000.0kJ/km^2
+#eco = create_eco(numSpecies, grd, area, totalK, req, individuals)
+#abun = runsim(eco, 100years, 1)
+#JLD.save("data/UK_example.jld", "abun", abun)
+abun = JLD.load("data/UK_example.jld", "abun")
+
+
+# Plot abundance over time for twenty species
+plot_abun(abun, grd, 1:20, 1)
+
+# Plot species richness over time
+heatmap(mapslices(x->sum(x.>0), abun, dims = 1)[1,:,:,1], xlab = "Time (months)", ylab = "Space")
+
+# Animation abundance over time for first species
+for i in 1:50:1200
+    display(heatmap(reshape(abun[1,:,i,1], 30, 10), clim = (0, 10)))
+end
+
+# Animation alpha bar and gamma
+for i in 1:50:1200
+    meta = Metacommunity(abun[:,:,i,1])
+    b = reshape(subdiv(ᾱ(meta), 1)[:diversity], 30, 10)
+    B = metadiv(γ(meta), 0)[:diversity]
+    if i ==1
+        display(heatmap(b, layout = (2, 1), clim = (1000, 3000), title = "Subcommunity ᾱ"))
+    else
+        display(heatmap!(b, subplot = 1,  clim = (1000, 3000)))
+    end
+    display(plot!([i], B, seriestype = :scatter, subplot = 2, xlim = (0, 1200), ylim = (2800, 3000), grid = false, label = "", color = :1, title = "Metacommunity γ"))
+end
+# Animation beta bar over time
+for i in 1:50:1200
+    meta = Metacommunity(abun[:,:,i,1])
+    b = reshape(subdiv(β̄(meta), 1)[:diversity], 30, 10)
+    B = metadiv(β̄(meta), 1)[:diversity]
+    if i ==1
+        display(heatmap(b, layout = (2, 1), clim = (1, 3), title = "Subcommunity β̄"))
+    else
+        display(heatmap!(b, subplot = 1,  clim = (1, 3)))
+    end
+    display(plot!([i], B, seriestype = :scatter, subplot = 2, xlim = (0, 1200), ylim = (1, 3), grid = false, label = "", title = "Metacommunity β̄"))
+end
+
+## 1,000 species on 100 by 100 grid for 1C - what do clusters look like?
+numSpecies = 1000; grd = (30,30); req = 100.0kJ; individuals = 2000000; area = 200000.0*km^2; totalK = 1000.000001kJ/km^2
+#eco = create_eco(numSpecies, grd, area, totalK, req, individuals)
+#abun = runsim(eco, 100years, 1)
+#JLD.save("data/Larger_grid_example.jld", "abun", abun)
+abun = JLD.load("data/Larger_grid_example.jld", "abun")
+
+# Plot species richness over time
+heatmap(mapslices(x->sum(x.>0), abun, dims = 1)[1,:,:,1], xlab = "Time (months)", ylab = "Space")
+
+# Animation abundance over time for first species
+for i in 1:20:1200
+    display(heatmap(reshape(abun[1,:,i,1], 30, 30), clim = (0, 10)))
+end
+
+using Simulation
+using Unitful
+using Unitful.DefaultSymbols
+#using Profile
+#using ProfileView
+using BenchmarkTools
+include("Documents/PhD/GIT/ClimatePref/examples/Met_funcs.jl");
+#numSpecies = 100; grd = (30,10); req = 100.0kJ; individuals = 100000; area = 200000.0*km^2; totalK = 1000.0kJ/km^2;
+#numSpecies = 200000; grd = (10,10); req= 10.0kJ; individuals=5000000; area = (250.0*km)^2; totalK = 1000.0kJ/km^2
+numSpecies = 3000; grd = (30,10); req = 100.0kJ; individuals = 1000000; area = 200000.0*km^2; totalK = 1000.0kJ/km^2
+eco = create_eco(numSpecies, grd, area, totalK, req, individuals);
+@benchmark abun = runsim(eco, 1month, 1)
+Profile.clear_malloc_data()
+@profile abun = runsim(eco, 1year, 1);
+ProfileView.view()
+
+# Africa example
+numSpecies = 50_000; grd = (300,150); req= 10.0kJ; individuals=5_000_000; area = 300_000_000.0*km^2; totalK = 1000.0kJ/km^2
