493dd75ffdc24edb112e7ca83f6e077dee9ff2de
Claire Harris <c.harris.2@research.gla.ac.uk>
2019-02-27 16:21:42 +0000

    ClimatePref.jl: Change new file names and only call in ERA interim tools if PyCall is loaded.

--- src/ClimatePref.jl
+++ src/ClimatePref.jl
@@ -1,20 +1,28 @@
 module ClimatePref
 
-include("DataTypes.jl")
+using Requires
+function __init__()
+    @require PyCall="438e738f-606a-5dbb-bf0a-cddfbfd45ab0" begin
+        println("Creating ECMWF interface ...")
+        include("ERA_interim_tools.jl")
+        export retrieve_era_interim
+    end
+end
+
+include("ClimateTypes.jl")
 export Worldclim, Bioclim, ERA, CERA, Reference
 
-include("GDAL.jl")
-export read, searchdir, extractworldclim, extractbioclim, extractERA, extractCERA,
- extractvalues, extractfile
+include("ReadData.jl")
+export read, searchdir, readworldclim, readbioclim, readERA, readCERA, readfile
 
-include("Tools.jl")
+include("ExtractClimate.jl")
+export extractvalues
+
+include("DataCleaning.jl")
 export create_reference, gardenmask, genus_worldclim_average,
     genus_worldclim_monthly, upresolution, getprofile
 
-include("Phylo_models.jl")
+include("PhyloModels.jl")
 export Brownian, Lambda, fitBrownian, fitLambda, varcovar
-
-include("drop_tip.jl")
-export getinternalnodes, drop_tip!
 
 end
