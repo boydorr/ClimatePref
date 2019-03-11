e97b8ee200aab28c8415db1e0d587201cdccd8c4
Claire Harris <c.harris.2@research.gla.ac.uk>
2019-03-11 11:57:17 +0000

    ERA_conversion.jl: Update times to reflect new 1979 - 2018 timeframe.

--- examples/ERA_conversion.jl
+++ examples/ERA_conversion.jl
@@ -4,10 +4,10 @@ using JuliaDB
 folder = "."
 files = ["era_int_netsolar", "era_int_soiltemp1", "era_int_temp2m", "era_int_totalprec"]
 params = ["ssr", "stl1", "t2m", "tp"]
-times = [collect(1980year:1month:(1990year - 1.0month)),
+times = [collect(1979year:1month:(1980year - 1.0month)),
+    collect(1980year:1month:(1990year - 1.0month)),
     collect(1990year:1month:(2000year - 1.0month)),
     collect(2000year:1month:(2010year - 1.0month)),
-    collect(2010year:1month:(2018year- 1.0month))]
 for i in eachindex(files)
     era = readERA(folder, files[i], params[i], times)
     eraDB = era_to_DB(era)
