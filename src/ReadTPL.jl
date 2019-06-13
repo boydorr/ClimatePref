1f3a1867f9c93e5f97e438f0c0fed12162a7e172
Claire Harris <c.harris.2@research.gla.ac.uk>
2019-06-13 13:41:14 +0100

    ReadTPL.jl: Create function called ReadTPL that can read in TPL data from folder as JuliaDB table.

--- src/ReadTPL.jl
+++ src/ReadTPL.jl
@@ -1,19 +1,17 @@
-
+using JuliaDB
 tpldict = Dict(:ID => String,	:Major_group => String,	:Family => String,	:Genus_hybrid_marker => String,	:Genus => String,	:Species_hybrid_marker => String,	:Species => String,	:Infraspecific_rank => String,	:Infraspecific_epithet => String,	:Authorship => String,	:Taxonomic_status_in_TPL => String,	:Nomenclatural_status_original => String,	:Confidence_level => String,	:Source => String,	:Source_id => String,	:IPNI_id => String,	:Publication => String,	:Collation => String,	:Page => String,	:Date => String)
 tplnames = ["ID",	"Major_group",	"Family",	"Genus_hybrid_marker",	"Genus",	"Species_hybrid_marker",	"Species",	"Infraspecific_rank",	"Infraspecific_epithet",	"Authorship",	"Taxonomic_status_in_TPL",	"Nomenclatural_status_original",	"Confidence_level",	"Source",	"Source_id",	"IPNI_id",	"Publication",	"Collation",	"Page",	"Date"]
-tpl = loadtable("TPL", header_exists = false, colnames = tplnames, colparsers = tpldict)
-tpl = filter(t-> t.Taxonomic_status_in_TPL == "Accepted" && t.Infraspecific_rank == "", tpl)
-duplicates = ["kew-2623303", "kew-2777599"]
-tpl = filter(t -> t.ID ∉ duplicates, tpl)
-spp = collect(select(tpl, :Species))
-gen = collect(select(tpl, :Genus))
-Species_name = gen .* " " .* spp
-tpl = pushcol(tpl, :Species_name, Species_name)
-save(tpl, "ThePlantList")
-
-
-using JuliaDB
-tpl = load("ThePlantList")
-gbif = load("GBIF/Era_GBIF")
-tpl = pushcol(tpl, :SppID, 1:length(tpl))
-gbif_tpl = join(gbif, tpl, how = :inner, lkey = :species, rkey = :Species_name, rselect = :SppID)
+function ReadTPL(folder::String)
+    tpl = loadtable(folder, header_exists = false, colnames = tplnames, colparsers = tpldict, chunks = 1)
+    tpl = filter(t-> t.Taxonomic_status_in_TPL == "Accepted" && t.Infraspecific_rank == "", tpl)
+    duplicates = ["kew-2623303", "kew-2777599"]
+    tpl = filter(t -> t.ID ∉ duplicates, tpl)
+    spp = collect(select(tpl, :Species))
+    gen = collect(select(tpl, :Genus))
+    Species_name = gen .* " " .* spp
+    tpl = pushcol(tpl, :species, Species_name)
+    tpl = pushcol(tpl, :SppID, 1:length(tpl))
+    tpl = reindex(tpl, :species)
+    tpl = popcol(tpl, :Species)
+    return tpl
+end
