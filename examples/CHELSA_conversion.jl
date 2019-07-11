322565812100438a1bff652f958397eb69bc5497
Claire Harris <c.harris.2@research.gla.ac.uk>
2019-07-11 22:40:59 +0100

    CHELSA_conversion.jl: Code for converting CHELSA files into JuliaDB tables.

---
+++ examples/CHELSA_conversion.jl
@@ -0,0 +1,22 @@
+using ClimatePref
+using Statistics
+using JuliaDB
+# Download CHELSA data from online server
+# for var in ["prec", "tmean", "tmax", "tmin"]
+#     for i in 1979:2013
+#         for j in 1:12
+#             j = @sprintf "%02d" j
+#             download("https://www.wsl.ch/lud/chelsa/data/timeseries/$var/CHELSA_$var" *"_$i" *"_$j" *"_V1.2.1.tif", "/home/claireh/Documents/CHELSA/ts/$var/CHELSA_$var" *"_$i" *"_$j.tif")
+#         end
+#     end
+# end
+
+# Read in data and save to JuliaDB
+dir = "CHELSA/ts"
+varlist = ["prec", "tmax", "tmean", "tmin"]
+aggfun = [sum, mean, mean, mean]
+for i in eachindex(varlist)
+    chelsa = readCHELSA(joinpath(dir, varlist[i]), varlist[i], res = 10, fn = aggfun[i])
+    chDB = ClimatePref.CHELSA_to_DB(chelsa)
+    JuliaDB.save(chDB, joinpath(dir, varlist[i] * "DB"))
+end
