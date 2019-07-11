158d74538a279d065dd4bec32e14a70334f7a2de
Claire Harris <c.harris.2@research.gla.ac.uk>
2019-07-11 22:41:37 +0100

    GBIF_workflow.jl: Code for getting GBIF data into Julia and processing for join with Plant list.

---
+++ examples/GBIF_workflow.jl
@@ -0,0 +1,11 @@
+using ClimatePref
+using JuliaDB
+gbif = ReadGBIF("final")
+save(gbif, "Full_GBIF")
+
+gbif = filter(g -> !ismissing(g.decimallatitude) & !ismissing(g.decimallongitude), gbif)
+save(gbif, "Geo_GBIF")
+
+ref = create_reference(0.75)
+gbif = extractvalues(gbif, ref, :refid)
+save(gbif, "Era_GBIF")
