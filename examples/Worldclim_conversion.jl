a3cc11c71ae0012ac775adbe2dd89a2d6790c717
Claire Harris <c.harris.2@research.gla.ac.uk>
2019-07-11 22:42:38 +0100

    Worldclim_conversion.jl: Convert worldclim files to JuliaDB tables.

--- examples/Worldclim_conversion.jl
+++ examples/Worldclim_conversion.jl
@@ -13,3 +13,19 @@ srad = readworldclim(joinpath(dir, "wc2.0_5m_srad"))
 vapr = readworldclim(joinpath(dir, "wc2.0_5m_vapr"))
 wind = readworldclim(joinpath(dir, "wc2.0_5m_wind"))
 bio = readbioclim(joinpath(dir, "wc2.0_5m_bio"))
+
+
+worldclim_to_DB(tavg)
+
+
+using ClimatePref
+using MyUnitful
+using JuliaDB
+folder = "wc2.0_5m"
+files = ["wc2.0_5m_tavg", "wc2.0_5m_tmax", "wc2.0_5m_tmin", "wc2.0_5m_prec", "wc2.0_5m_srad", "wc2.0_5m_vapr", "wc2.0_5m_wind"]
+params = ["tavg", "tmax", "tmin", "prec", "srad", "vapr", "wind"]
+for i in eachindex(files)
+    wc = readworldclim(joinpath(folder, files[i]))
+    wcDB = worldclim_to_DB(wc)
+    save(wcDB, joinpath("Worldclim", params[i]))
+end
